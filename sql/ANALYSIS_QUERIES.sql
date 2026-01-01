3cddfba9e86fb6dd2592ea0963c7002349f81518
Andy R <programmerandy@gmail.com>
2025-12-31 21:19:40 -0500

    moved data transformation to package for now

--- sql/ANALYSIS_QUERIES.sql
+++ sql/ANALYSIS_QUERIES.sql
@@ -1,85 +1,33 @@
 --Analysis by account
 select acct.institution,
        acct.account_name,
-       CASE
-           when upper(trans.description) like '%PAYMENT%' and trans.amount > 0
-               then 'CC Payment'
-           when upper(trans.description) like '%DOORDASH%' then 'Doordash'
-           when upper(trans.description) like '%WALMART%' or upper(trans.description) like '%WM SUPERCENTER%' then 'Walmart'
-           when upper(trans.description) like '%EVERYDAY%' then 'Everyday App'
-           when upper(trans.description) like '%COVETRUS%' then 'Covetrus - Pet Meds'
-           when upper(trans.description) like '%FIGO%' then 'Figo - Pet Insurance'
-           when upper(trans.description) like '%LIFEPOINT%' then 'Lifepoint - Tithes'
-           when upper(trans.description) like '%KROGER%' then 'Kroger'
-           when upper(trans.description) like '%NATIONWIDE%' then 'Nationwide - Andy Payroll'
-           when upper(trans.description) like '%CHEWY%' then 'Chewy - Pet Supplies'
-           when upper(trans.description) like '%AMAZON%' and trans.amount > 0 then 'Bridget - Amazon Payroll'
-           when upper(trans.description) like '%COMPONE%' and trans.amount > 0 then 'Bridget - Absence Plus Payroll'
-           when upper(trans.description) like '%CARDINAL LAWNS%' then 'Cardinal Lawns'
-           else trans.description
-       END as normalized_description,
+       trans.merchant_tag,
        count(*) as count_transactions,
        sum(trans.amount) as total_transactions
 from transactions trans
 inner join accounts acct on acct.account_id = trans.account_id
 group by acct.institution,
          acct.account_name,
-         normalized_description
+         normaliztrans.merchant_taged_description
 order by count_transactions desc;
 
 
 --analysis by top count of payments
-select CASE
-           when upper(trans.description) like '%PAYMENT%' and trans.amount > 0
-               then 'CC Payment'
-           when upper(trans.description) like '%DOORDASH%' then 'Doordash'
-           when upper(trans.description) like '%WALMART%' or upper(trans.description) like '%WM SUPERCENTER%' then 'Walmart'
-           when upper(trans.description) like '%EVERYDAY%' then 'Everyday App'
-           when upper(trans.description) like '%COVETRUS%' then 'Covetrus - Pet Meds'
-           when upper(trans.description) like '%FIGO%' then 'Figo - Pet Insurance'
-           when upper(trans.description) like '%LIFEPOINT%' then 'Lifepoint - Tithes'
-           when upper(trans.description) like '%KROGER%' then 'Kroger'
-           when upper(trans.description) like '%NATIONWIDE%' then 'Nationwide - Andy Payroll'
-           when upper(trans.description) like '%CHEWY%' then 'Chewy - Pet Supplies'
-           when upper(trans.description) like '%AMAZON%' and trans.amount > 0 then 'Bridget - Amazon Payroll'
-           when upper(trans.description) like '%COMPONE%' and trans.amount > 0 then 'Bridget - Absence Plus Payroll'
-           when upper(trans.description) like '%CARDINAL LAWNS%' then 'Cardinal Lawns'
-           else trans.description
-       END as normalized_description,
+select trans.merchant_tag,
        count(*) as count_transactions,
        sum(trans.amount) as total_transactions
 from transactions trans
 inner join accounts acct on acct.account_id = trans.account_id
-group by normalized_description
+group by trans.merchant_tag
 order by count_transactions desc;
 
 --analysis by top total paid
-select CASE
-           when upper(trans.description) like '%PAYMENT%' and trans.amount > 0
-               then 'CC Payment'
-           when upper(trans.description) like '%DOORDASH%' then 'Doordash'
-           when upper(trans.description) like '%WALMART%' or upper(trans.description) like '%WM SUPERCENTER%' or upper(trans.description) like '%WAL-MART%' then 'Walmart'
-           when upper(trans.description) like '%EVERYDAY%' then 'Everyday App'
-           when upper(trans.description) like '%COVETRUS%' then 'Covetrus - Pet Meds'
-           when upper(trans.description) like '%FIGO%' then 'Figo - Pet Insurance'
-           when upper(trans.description) like '%LIFEPOINT%' then 'Lifepoint - Tithes'
-           when upper(trans.description) like '%KROGER%' then 'Kroger'
-           when (upper(trans.description) like '%NATIONWIDE%') or (upper(trans.description) like '%AMAZON%' and trans.amount > 0) or upper(trans.description) like '%COMPONE%' and trans.amount > 0 then 'Payroll'
-           when upper(trans.description) like '%CHEWY%' then 'Chewy - Pet Supplies'
-           when upper(trans.description) like '%CARDINAL LAWNS%' then 'Cardinal Lawns'
-           when upper(trans.description) like '%COSTCO TRAVEL%' then 'COSTCO Travel'
-           when upper(trans.description) like '%COSTCO WHSE%' then 'COSTCO'
-           when upper(trans.description) like '%DIVIDEND DEPOSIT%' or upper(trans.description) like '%ANNUAL PERCENTAGE YIELD EARNED%' or upper(trans.description) like '%DIVIDEND DEPOSIT%' then 'Account Interest earnings'
-           when upper(trans.description) like '%WORTHINGTON WOODS%' then 'Worthington Woods - Vet'
-           when upper(trans.description) like '%AMAZON%' and trans.amount < 0 then 'Amazon Marketplace'
-           when upper(trans.description) like '%BLACK WING%' or upper(trans.description) like '%CARDINAL CENTER%'  then 'Sporting Clays'
-           else trans.description
-       END as normalized_description,
+select trans.merchant_tag,
        count(*) as count_transactions,
        sum(trans.amount) as total_transactions,
        abs(sum(trans.amount)) as absolute_total_transactions
 from transactions trans
 inner join accounts acct on acct.account_id = trans.account_id
-group by normalized_description
+group by trans.merchant_tag
 HAVING COUNT(*) > 1
 order by absolute_total_transactions desc;
